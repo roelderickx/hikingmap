8f02628fccba96d751d0c07b3bbda982b842eee9
roelderickx <roel.derickx@gmail.com>
2019-09-21 13:33:33 +0200

    Script to add NASA's 1-arc geotiff files to the database

--- elevation/import_in_db.sh
+++ elevation/import_in_db.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+GIS_DB=gis
+GIS_USER=gis
+TABLE_NAME=elevation
+CREATE_TABLE=1
+
 rm *.shp *.shx *.dbf *.index *.prj *.sql
 
 for X in *.hgt
@@ -8,11 +13,25 @@ do
 
     shapeindex ${X%%.hgt}c10.shp
     
-    if [ ! -e elevation.sql ]; then
-        shp2pgsql -p -s 4326 -I ${X%%.hgt}c10.shp elevation > elevation.sql
+    if [ $CREATE_TABLE -eq 1 ]; then
+        shp2pgsql -c -D -s 4326 -i ${X%%.hgt}c10.shp $TABLE_NAME >> elevation.sql
+        CREATE_TABLE=2
+    else
+        shp2pgsql -a -D -s 4326 -i ${X%%.hgt}c10.shp $TABLE_NAME >> elevation.sql
     fi
-    shp2pgsql -a -D -s 4326 -i ${X%%.hgt}c10.shp elevation >> elevation.sql
+    
+    # disable analyze after each insert to improve performance
+    sed -i -e 's/ANALYZE/--ANALYZE/g' elevation.sql
+    # insert data
+    psql -d $GIS_DB -U $GIS_USER -f elevation.sql
+    
+    rm *.shp *.shx *.dbf *.index *.prj *.sql
 done
 
-psql -d gis -U gis -f elevation.sql
+# analyze table
+echo "ANALYZE \"$TABLE_NAME\"" | psql -d $GIS_DB -U $GIS_USER
+# create the index we omitted in the beginning (shp2pgsql -c in stead of shp2pgsql -c -I)
+if [ $CREATE_TABLE -eq 2 ]; then
+    echo "CREATE INDEX ON \"$TABLE_NAME\" USING GIST (\"geom\");" | psql -d $GIS_DB -U $GIS_USER
+fi
 
